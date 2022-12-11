1ce49cd9568f1b0489b7f5800390b70d5f2c3ae8
Jerry Okolo <jerrycreative@live.co.uk>
2022-12-11 18:52:50 +0100

    complete Metadata entity with proper types

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
+
 use crate::pb::cryptopunks as punks;
 
-use substreams::store::DeltaString;
+use substreams::scalar::BigInt;
 use substreams_entity_change::pb::entity::{entity_change::Operation, EntityChanges};
 
 use substreams::store::{DeltaProto, Deltas};
@@ -8,64 +10,85 @@ use substreams::store::{DeltaProto, Deltas};
 // -------------------
 //  Map Metadata Entities
 // -------------------
+
+//CREATE
 pub fn store_metadata_entity_change(
     entity_changes: &mut EntityChanges,
     deltas: Deltas<DeltaProto<punks::Metadata>>,
 ) {
     for delta in deltas.deltas {
-        let punk_id = delta.key.as_str().split(":").last().unwrap();
+        let punk_id = delta.key.as_str().split(":").last().unwrap().trim();
 
         entity_changes
             .push_change("MetaData", punk_id, delta.ordinal, Operation::Create)
-            .change(
-                "tokenId",
-                DeltaString {
-                    operation: delta.operation,
-                    ordinal: 0,
-                    key: punk_id.to_string(),
-                    old_value: delta.old_value.token_id,
-                    new_value: delta.new_value.token_id,
-                },
-            )
-            .change(
-                "tokenURI",
-                DeltaString {
-                    operation: delta.operation,
-                    ordinal: 0,
-                    key: punk_id.to_string(),
-                    old_value: delta.old_value.token_uri,
-                    new_value: delta.new_value.token_uri,
-                },
-            )
-            .change(
-                "image",
-                DeltaString {
-                    operation: delta.operation,
-                    ordinal: 0,
-                    key: punk_id.to_string(),
-                    old_value: delta.old_value.image,
-                    new_value: delta.new_value.image,
-                },
-            )
-            .change(
-                "contractURI",
-                DeltaString {
-                    operation: delta.operation,
-                    ordinal: 0,
-                    key: punk_id.to_string(),
-                    old_value: delta.old_value.contract_uri,
-                    new_value: delta.new_value.contract_uri,
-                },
-            )
-            .change(
-                "traits",
-                DeltaString {
-                    operation: delta.operation,
-                    ordinal: 0,
-                    key: punk_id.to_string(),
-                    old_value: delta.old_value.traits,
-                    new_value: delta.new_value.traits,
-                },
-            );
+            .change("id", delta.new_value.token_id)
+            .change("tokenId", BigInt::from_str(&punk_id).unwrap())
+            .change("tokenURI", delta.new_value.token_uri)
+            .change("image", delta.new_value.image)
+            .change("svg", delta.new_value.svg)
+            .change("contractURI", delta.new_value.contract_uri)
+            .change("traits", delta.new_value.traits);
     }
 }
+
+//UPDATE
+// pub fn store_metadata_entity_change(
+//     entity_changes: &mut EntityChanges,
+//     deltas: Deltas<DeltaProto<punks::Metadata>>,
+// ) {
+//     for delta in deltas.deltas {
+//         let punk_id = delta.key.as_str().split(":").last().unwrap().trim();
+
+//         entity_changes
+//             .push_change("MetaData", punk_id, delta.ordinal, Operation::Create)
+//             .change(
+//                 "tokenId",
+//                 DeltaString {
+//                     operation: delta.operation,
+//                     ordinal: 0,
+//                     key: punk_id.to_string(),
+//                     old_value: delta.old_value.token_id,
+//                     new_value: delta.new_value.token_id,
+//                 },
+//             )
+//             .change(
+//                 "tokenURI",
+//                 DeltaString {
+//                     operation: delta.operation,
+//                     ordinal: 0,
+//                     key: punk_id.to_string(),
+//                     old_value: delta.old_value.token_uri,
+//                     new_value: delta.new_value.token_uri,
+//                 },
+//             )
+//             .change(
+//                 "image",
+//                 DeltaString {
+//                     operation: delta.operation,
+//                     ordinal: 0,
+//                     key: punk_id.to_string(),
+//                     old_value: delta.old_value.image,
+//                     new_value: delta.new_value.image,
+//                 },
+//             )
+//             .change(
+//                 "contractURI",
+//                 DeltaString {
+//                     operation: delta.operation,
+//                     ordinal: 0,
+//                     key: punk_id.to_string(),
+//                     old_value: delta.old_value.contract_uri,
+//                     new_value: delta.new_value.contract_uri,
+//                 },
+//             )
+//             .change(
+//                 "traits",
+//                 DeltaString {
+//                     operation: delta.operation,
+//                     ordinal: 0,
+//                     key: punk_id.to_string(),
+//                     new_value: delta.new_value.traits,
+//                 },
+//             );
+//     }
+// }
